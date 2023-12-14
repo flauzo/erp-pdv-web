6652613423c11b6b3ce7b5b13b2485f06f1843bb
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-14 17:58:51 -0300

    Update GrupoUsuarioService.java

--- src/main/java/net/originmobi/pdv/service/GrupoUsuarioService.java
+++ src/main/java/net/originmobi/pdv/service/GrupoUsuarioService.java
@@ -32,7 +32,7 @@ public class GrupoUsuarioService {
 
 		if (grupoUsuario.getCodigo() == null) {
 			try {
-				attributes.addFlashAttribute("mensagem", "Grupo adicionado com sucesso");
+				attributes.addFlashAttribute("mensagem", "Grupo adicionado com sucesso.");
 				grupousuarios.save(grupoUsuario);
 			} catch (Exception e) {
 				System.out.println(e);
@@ -42,15 +42,14 @@ public class GrupoUsuarioService {
 			attributes.addFlashAttribute("mensagem", "Grupo atualizado com sucesso");
 			grupousuarios.update(grupoUsuario.getNome(), grupoUsuario.getDescricao(), grupoUsuario.getCodigo());
 		}
-
 	}
 
 	public String remove(Long codigo, RedirectAttributes attributes) {
-		// verifico se o grupo esta vinculado a algum usuário
+		// verifico se o Grupo esta vinculado a algum usuário.
 		int aux = grupousuarios.grupoTemUsuaio(codigo);
 
 		if (aux > 0) {
-			attributes.addFlashAttribute("mensagemErro", "Este grupo possue usuários vinculados a ele, verifique");
+			attributes.addFlashAttribute("mensagemErro", "Este Grupo possue usuários vinculados a ele, verifique.");
 			return "redirect:/grupousuario/" + codigo;
 		}
 
@@ -68,13 +67,13 @@ public class GrupoUsuarioService {
 		int aux = grupousuarios.grupoTemPermissao(codgrupo, codpermissao);
 		
 		if (aux > 0)
-			throw new RuntimeException("Esta permissão já esta adicionada a este grupo");
+			throw new RuntimeException("Esta Permissão já esta adicionada a este Grupo.");
 
 		try {
 			grupousuarios.addPermissao(codgrupo, codpermissao);
 		} catch (Exception e) {
 			System.out.println(e);
-			throw new RuntimeException("Erro ao tentar adicionar permissão, chame o suporte");
+			throw new RuntimeException("Erro ao tentar Adicionar Permissão, chame o suporte.");
 		}
 
 		return "Permissao adicionada com sucesso";
@@ -84,9 +83,8 @@ public class GrupoUsuarioService {
 		try {
 			grupousuarios.removePermissao(codigo, codgrupo);
 		} catch (Exception e) {
-			throw new RuntimeException("Erro ao tentar remover permissão, chame o suporte");
+			throw new RuntimeException("Erro ao tentar Remover Permissão, chame o suporte.");
 		}
-		return "Permissão removida com sucesso";
+		return "Permissão removida com sucesso.";
 	}
-
 }
