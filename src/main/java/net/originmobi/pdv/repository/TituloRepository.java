2c5aeda57f6ecf6329063e1e902023fd332b2d53
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 17:58:25 -0300

    Update TituloRepository.java

--- src/main/java/net/originmobi/pdv/repository/TituloRepository.java
+++ src/main/java/net/originmobi/pdv/repository/TituloRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Titulo;
 
+
 public interface TituloRepository extends JpaRepository<Titulo, Long> {
 
 }
