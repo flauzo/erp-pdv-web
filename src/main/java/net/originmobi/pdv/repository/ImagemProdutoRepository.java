f2789b1b9bd9eb5d10b1c06ccd231ea73ed37bc9
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:36:40 -0300

    Update ImagemProdutoRepository.java

--- src/main/java/net/originmobi/pdv/repository/ImagemProdutoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/ImagemProdutoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.ImagemProduto;
 
+
 public interface ImagemProdutoRepository extends JpaRepository<ImagemProduto, Long> {
 	
 	public ImagemProduto findByProdutoCodigo(Long codigo);
