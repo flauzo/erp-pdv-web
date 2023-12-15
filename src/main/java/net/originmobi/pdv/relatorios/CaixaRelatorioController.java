401a2325e164cf3c8716ab6f4f25eadbdde6b5fa
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 19:26:21 -0300

    Update CaixaRelatorioController.java

--- src/main/java/net/originmobi/pdv/relatorios/CaixaRelatorioController.java
+++ src/main/java/net/originmobi/pdv/relatorios/CaixaRelatorioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/caixa/relatorio")
+
 public class CaixaRelatorioController {
 	
 	private GerarRelatorio relatorio;
