46babf3d91428e17522ab414dad9f4622229bd09
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:20:35 -0300

    Update PagarParcelaRespository.java

--- src/main/java/net/originmobi/pdv/repository/PagarParcelaRespository.java
+++ src/main/java/net/originmobi/pdv/repository/PagarParcelaRespository.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import net.originmobi.pdv.model.Pagar;
 import net.originmobi.pdv.model.PagarParcela;
 
+
 public interface PagarParcelaRespository extends JpaRepository<PagarParcela, Long> {
 
 	@Transactional
