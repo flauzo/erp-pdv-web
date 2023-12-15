e397757da0bfce411f986cbd7e61c3196ce5fe8e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:49:52 -0300

    Update CstCsosnRepository.java

--- src/main/java/net/originmobi/pdv/repository/CstCsosnRepository.java
+++ src/main/java/net/originmobi/pdv/repository/CstCsosnRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.CstCsosn;
 
+
 public interface CstCsosnRepository extends JpaRepository<CstCsosn, Long>{
 
 }
