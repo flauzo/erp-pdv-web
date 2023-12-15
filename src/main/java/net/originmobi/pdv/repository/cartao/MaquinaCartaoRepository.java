0a16ae67e5b87a2e732437046a078b6e257c4f10
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 19:05:29 -0300

    Update MaquinaCartaoRepository.java

--- src/main/java/net/originmobi/pdv/repository/cartao/MaquinaCartaoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/cartao/MaquinaCartaoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.cartao.MaquinaCartao;
 
+
 public interface MaquinaCartaoRepository extends JpaRepository<MaquinaCartao, Long> {
 
 }
