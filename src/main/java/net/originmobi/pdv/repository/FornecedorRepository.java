b3f855e872177108f7355a40a6698cd6925009a7
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:40:32 -0300

    Update FornecedorRepository.java

--- src/main/java/net/originmobi/pdv/repository/FornecedorRepository.java
+++ src/main/java/net/originmobi/pdv/repository/FornecedorRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Fornecedor;
 
+
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
 	public List<Fornecedor> findByNomeContaining(String nome);
@@ -13,5 +14,4 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 	public Fornecedor findByCnpjIn(String cnpj);
 
 	public Fornecedor findByCodigoIn(Long codigo);
-
 }
