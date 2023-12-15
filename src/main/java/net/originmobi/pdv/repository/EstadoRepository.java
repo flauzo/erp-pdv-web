7a829b469b016eabcfd80bc8c545fb9d184f8e8b
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:41:17 -0300

    Update EstadoRepository.java

--- src/main/java/net/originmobi/pdv/repository/EstadoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/EstadoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Estado;
 
+
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
 }
