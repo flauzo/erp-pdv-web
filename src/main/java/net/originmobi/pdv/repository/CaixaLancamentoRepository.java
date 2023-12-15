be2156f084ed9ab555bb38a1aae085c51bd3eff8
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:54:10 -0300

    Update CaixaLancamentoRepository.java

--- src/main/java/net/originmobi/pdv/repository/CaixaLancamentoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/CaixaLancamentoRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import net.originmobi.pdv.model.Caixa;
 import net.originmobi.pdv.model.CaixaLancamento;
 
+
 public interface CaixaLancamentoRepository extends JpaRepository<CaixaLancamento, Long> {
 
 	public List<CaixaLancamento> findByCaixaEquals(Caixa caixa);
-
 }
