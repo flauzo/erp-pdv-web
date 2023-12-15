9a0e352b20497619362f4387734815779a528e8a
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:02:42 -0300

    Update RegimeTributarioRepository.java

--- src/main/java/net/originmobi/pdv/repository/RegimeTributarioRepository.java
+++ src/main/java/net/originmobi/pdv/repository/RegimeTributarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.RegimeTributario;
 
+
 public interface RegimeTributarioRepository extends JpaRepository<RegimeTributario, Long>{
 
 }
