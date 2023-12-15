c2712f38fa1bb6a2a1065472e2266c163e96fc1d
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:52:16 -0300

    Update CategoriaRepository.java

--- src/main/java/net/originmobi/pdv/repository/CategoriaRepository.java
+++ src/main/java/net/originmobi/pdv/repository/CategoriaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Categoria;
 
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
 }
