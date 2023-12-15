3b17c912d2c49ed1d98967036946eb374876001e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 17:53:36 -0300

    Update UsuarioRepository.java

--- src/main/java/net/originmobi/pdv/repository/UsuarioRepository.java
+++ src/main/java/net/originmobi/pdv/repository/UsuarioRepository.java
@@ -7,11 +7,9 @@ import net.originmobi.pdv.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Usuario findByPessoaCodigoEquals(Long codigo);
-
 	public Usuario findByUserEquals(String user);
 
 	public Usuario findByUserContaining(String usuario);
-
 	public Usuario findByCodigoIn(Long codigo);
 	
 }
