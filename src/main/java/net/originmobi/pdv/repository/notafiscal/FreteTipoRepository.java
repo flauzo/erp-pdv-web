2bb3f286d74bd4dae7823813434b07c92a36f5c6
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 19:03:44 -0300

    Update FreteTipoRepository.java

--- src/main/java/net/originmobi/pdv/repository/notafiscal/FreteTipoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/notafiscal/FreteTipoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.FreteTipo;
 
+
 public interface FreteTipoRepository extends JpaRepository<FreteTipo, Long> {
 
 }
