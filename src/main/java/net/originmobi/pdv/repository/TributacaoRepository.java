ca982ad539ef684f7790238c3ab6723cb1309b7e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 17:54:57 -0300

    Update TributacaoRepository.java

--- src/main/java/net/originmobi/pdv/repository/TributacaoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/TributacaoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Tributacao;
 
+
 public interface TributacaoRepository extends JpaRepository<Tributacao, Long> {
 
 }
