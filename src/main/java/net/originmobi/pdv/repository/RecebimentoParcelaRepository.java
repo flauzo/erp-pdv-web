2f165ef80572924cccbc0071d111ceed8057b077
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:04:32 -0300

    Update RecebimentoParcelaRepository.java

--- src/main/java/net/originmobi/pdv/repository/RecebimentoParcelaRepository.java
+++ src/main/java/net/originmobi/pdv/repository/RecebimentoParcelaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import net.originmobi.pdv.model.Parcela;
 import net.originmobi.pdv.model.RecebimentoParcela;
 
+
 public interface RecebimentoParcelaRepository extends JpaRepository<RecebimentoParcela, Long> {
 
 	@Query("select p from RecebimentoParcela rp, Parcela p where p.codigo = rp.parcela and rp.recebimento = ?1")
