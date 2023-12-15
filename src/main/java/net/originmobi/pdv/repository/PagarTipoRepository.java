616c4e2cc9fd54f0fee6873be9508ac2be6a7269
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:16:17 -0300

    Update PagarTipoRepository.java

--- src/main/java/net/originmobi/pdv/repository/PagarTipoRepository.java
+++ src/main/java/net/originmobi/pdv/repository/PagarTipoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.PagarTipo;
 
+
 public interface PagarTipoRepository extends JpaRepository<PagarTipo, Long> {
 
 }
