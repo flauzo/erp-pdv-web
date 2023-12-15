e5360d8099a7b68e3f644f51b66391c616aba1f9
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 18:00:05 -0300

    Update TipoAmbienteRepository.java

--- src/main/java/net/originmobi/pdv/repository/TipoAmbienteRepository.java
+++ src/main/java/net/originmobi/pdv/repository/TipoAmbienteRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.TipoAmbiente;
 
+
 public interface TipoAmbienteRepository extends JpaRepository<TipoAmbiente, Long> {
 
 }
