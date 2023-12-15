cb69d0dd76c90738580ab07c23e6a59eaab19bde
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-15 17:57:21 -0300

    Update TransferenciaRepository.java

--- src/main/java/net/originmobi/pdv/repository/TransferenciaRepository.java
+++ src/main/java/net/originmobi/pdv/repository/TransferenciaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.originmobi.pdv.model.Transferencia;
 
+
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 
 }
