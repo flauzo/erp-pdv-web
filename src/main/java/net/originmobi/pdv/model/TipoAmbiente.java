835d3e91a22bd017ef8abf54110c46ad87408e35
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-16 10:13:40 -0300

    Update TipoAmbiente.java

--- src/main/java/net/originmobi/pdv/model/TipoAmbiente.java
+++ src/main/java/net/originmobi/pdv/model/TipoAmbiente.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "tipo_ambiente")
 public class TipoAmbiente implements Serializable {
