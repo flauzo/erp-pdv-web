c5c40ea80ccdeaf6abd7ecdfd2ce385f5ecf8fe1
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-16 10:14:20 -0300

    Update Transferencia.java

--- src/main/java/net/originmobi/pdv/model/Transferencia.java
+++ src/main/java/net/originmobi/pdv/model/Transferencia.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
+
 @Entity
 public class Transferencia implements Serializable {
 
