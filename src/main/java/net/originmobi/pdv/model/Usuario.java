18172651c75049289b167d1e3d64fdab8a188955
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-16 10:17:28 -0300

    Update Usuario.java

--- src/main/java/net/originmobi/pdv/model/Usuario.java
+++ src/main/java/net/originmobi/pdv/model/Usuario.java
@@ -21,6 +21,7 @@ public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
