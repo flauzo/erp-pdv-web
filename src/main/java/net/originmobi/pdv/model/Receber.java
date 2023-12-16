ef8ff126c35b1696824312ab956d94a06d2902b7
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-16 10:20:28 -0300

    Update Receber.java

--- src/main/java/net/originmobi/pdv/model/Receber.java
+++ src/main/java/net/originmobi/pdv/model/Receber.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
+
 @Entity
 public class Receber implements Serializable {
 
