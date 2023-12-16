6054ee6a65fc1ec0927ba1846bc615449478cdd5
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-16 14:40:18 -0300

    Update PdvApplication.java

--- src/main/java/net/originmobi/pdv/PdvApplication.java
+++ src/main/java/net/originmobi/pdv/PdvApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+
 @SpringBootApplication
 public class PdvApplication extends SpringBootServletInitializer {
 
