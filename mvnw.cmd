4324bcebf3dba34a17000db4a6e0f43f689ecad1
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2023-12-17 17:41:05 -0300

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,20 +1,20 @@
 @REM ----------------------------------------------------------------------------
-@REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor license agreements.  See the NOTICE file
-@REM distributed with this work for additional information
-@REM regarding copyright ownership.  The ASF licenses this file
-@REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file except in compliance
-@REM with the License.  You may obtain a copy of the License at
+@REM Licenciado para a Apache Software Foundation (ASF) sob um
+@REM ou mais contratos de licença de contribuidor. Veja o arquivo AVISO
+@REM distribuído com este trabalho para informações adicionais
+@REM em relação à propriedade de direitos autorais. O ASF licencia este arquivo
+@REM para você sob a licença Apache, versão 2.0 (o
+@REM "Licença"); você não pode usar este arquivo exceto em conformidade
+@REM com a licença. Você pode obter uma cópia da Licença em
 @REM
 @REM    http://www.apache.org/licenses/LICENSE-2.0
 @REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
+@REM A menos que exigido pela lei aplicável ou acordado por escrito, O software 
+@REM distribuído sob a Licença é distribuído em um
+@REM "COMO ESTÁ", SEM GARANTIAS OU CONDIÇÕES DE QUALQUER
+@REM KIND, expresso ou implícito. Veja a Licença do Linguagem específica do 
+@REM que rege permissões e limitações
+@REM sob a licença.
 @REM ----------------------------------------------------------------------------
 
 @REM ----------------------------------------------------------------------------
